ae6daa79310dc22a1a27e9756d0711b6c0cd9de0
purcellchris <mitchelljack979@gmail.com>
2021-09-26 17:29:16 +0100

    one page version

--- assets/js/main-load-copyright.js
+++
@@ -1,9 +0,0 @@
-var copyrightText = "Cliff Olaw © 2021"
-
-window.onload = function() {
-    var x = document.getElementsByClassName("copyrights");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].innerHTML=copyrightText;
-    }
-} 
